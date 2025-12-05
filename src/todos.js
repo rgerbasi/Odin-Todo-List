3a8ded2efa373da72627bd6e87897531d3c641fe
rod <31828557+rgerbasi@users.noreply.github.com>
2025-12-05 16:07:18 -0500

    add: date-fns and todos module

--- src/todos.js
+++ src/todos.js
@@ -1,6 +1,30 @@
 // todos.js
+import { format, } from "date-fns"
+
 
 export class ToDoItem {
 
+    title;
+    description;
+    dueDate;
+    priority;
+    notes;
+    checklist;
+
+    constructor (properties = {}) {
+        this.title = properties.title ?? "";
+        this.description = properties.description ?? "";
+        this.dueDate = properties.dueDate ?? new Date();
+        this.priority = properties.priority;
+    }
     
+
+}
+
+export class Project {
+    todos = [];
+
+    constructor (properties = {}) {
+        this.todos = properties.todos;
+    }
 }
